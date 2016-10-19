b85860aa69648a5a801d39127af8474e32a1057e
mathuramg <mmg9291@gmail.com>
2016-10-19 12:36:03 -0400

    add lesson-tag models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017145124) do
+ActiveRecord::Schema.define(version: 20161019140017) do
+
+  create_table "lessons", force: :cascade do |t|
+    t.string   "name"
+    t.text     "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lessons_tags", id: false, force: :cascade do |t|
+    t.integer "lesson_id", null: false
+    t.integer "tag_id",    null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +36,12 @@ ActiveRecord::Schema.define(version: 20161017145124) do
   create_table "students_users", id: false, force: :cascade do |t|
     t.integer "user_id",    null: false
     t.integer "student_id", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
