24e54fb2329f38d17a86f921778df035a07f8229
mathuramg <mmg9291@gmail.com>
2016-11-09 23:39:53 -0500

    add dropbox

--- Gemfile
+++ Gemfile
@@ -34,7 +34,8 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
-gem "paperclip", "~> 5.0.0"
+gem "paperclip", "~> 3.1"
+gem "paperclip-dropbox", ">= 1.1.7"
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,5 +1,7 @@
 class Audio < ApplicationRecord
   has_and_belongs_to_many :students
-  has_attached_file :file
-  do_not_validate_attachment_file_type :file
+  has_attached_file :file,
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  # do_not_validate_attachment_file_type :file
 end
