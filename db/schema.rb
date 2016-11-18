c3d3cb740bce51dddcab9c7ac9d0719c23663e6a
mathuramg <mmg9291@gmail.com>
2016-11-17 21:05:43 -0500

    add colour to tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110054838) do
+ActiveRecord::Schema.define(version: 20161118012350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20161110054838) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "tag_color"
+    t.string   "color"
   end
 
   create_table "users", force: :cascade do |t|
