11153f171bb03484b902f7206695717626c99bcc
mathuramg <mmg9291@gmail.com>
2016-12-04 00:05:52 -0500

    add comments to class

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,3 +1,4 @@
 class Section < ApplicationRecord
+  has_and_belongs_to_many :comments
   has_and_belongs_to_many :students
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161204041454) do
+ActiveRecord::Schema.define(version: 20161204045314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 20161204041454) do
     t.text     "comments_text"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "comments_sections", id: false, force: :cascade do |t|
+    t.integer "comment_id", null: false
+    t.integer "section_id", null: false
   end
 
   create_table "comments_students", id: false, force: :cascade do |t|
